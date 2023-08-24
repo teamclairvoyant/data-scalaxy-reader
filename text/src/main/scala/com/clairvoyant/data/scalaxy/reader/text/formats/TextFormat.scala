1d6a05ba62dba04fc6133bd5544d9c0585280c1d
rahulbhatia023 <rahul.bhatia023@gmail.com>
2023-08-24 15:49:08 -0400

    Code Improvement

--- text/src/main/scala/com/clairvoyant/data/scalaxy/reader/text/formats/TextFormat.scala
+++ text/src/main/scala/com/clairvoyant/data/scalaxy/reader/text/formats/TextFormat.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.types.StructType
 import zio.config.derivation.*
 
 @nameWithLabel
-trait TextFormat
+sealed trait TextFormat
 
 case class CSVTextFormat(
     adaptSchemaColumns: StructType => StructType = identity,
